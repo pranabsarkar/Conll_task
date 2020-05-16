c2983d080cc5baf9629682d7d30c92db62ff07e5
Pranab Sarkar <sarkarpranab66@gmail.com>
2020-05-16 21:20:48 +0530

    Add files via upload

--- eval.py
+++ eval.py
@@ -14,20 +14,7 @@ def find_metrics(X_teste, y_teste,model_path1,all_words,all_tags):
     preds, y_true = np.argmax(model.predict(X_teste), axis=-1), np.argmax(y_teste, -1)
     pred_tag, true_tag = parser2categorical(preds, y_true, all_tags) 
     print(metrics(pred_tag, true_tag))
-    index = 100
-
-    print("{:15}   {:5}:   [{}]".format("Word", "True", "Pred"))
-    print("=" * 35)
-
-    k = tag2idx(all_words)
-    word_parser_idx = idx2tag(k)
-
-    for word, true, pred in zip([word_parser_idx[x] for x in X_teste[index]], 
-                                true_tag[index], 
-                                pred_tag[index]):
-
-        if pred != 'PAD':
-            print("{:15}   {:5}    [{}]".format(word, true, pred))
+    
 
 def POS_EVAL():
     with open('encoding/X_teste_POS.pkl', 'rb') as f:
@@ -78,12 +65,5 @@ def NER_EVAL():
     
     find_metrics(X_teste, y_teste,PATH1A,all_words,all_tags)
 
-def main():
-    print("-----------------------POS TAGGING EVAL---------------------------")
-    POS_EVAL()
-    print("-----------------------CHUNK TAGGING EVAL---------------------------")
-    PAR_EVAL()
-    print("-----------------------NER TAGGING EVAL---------------------------")
-    NER_EVAL()
 
-main()
+
